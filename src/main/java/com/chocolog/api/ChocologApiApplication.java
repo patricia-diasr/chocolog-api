c5feaa1da5bc9b5f0ac5812b3ff7bd88ab2a4a14
Patrícia Dias Rodrigues <125562670+patricia-diasr@users.noreply.github.com>
2025-10-06 11:17:08 +0000

    chore: inicializar e configurar projeto Spring Boot

---
+++ src/main/java/com/chocolog/api/ChocologApiApplication.java
@@ -0,0 +1,13 @@
+package com.chocolog.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChocologApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChocologApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chocolog/api/ChocologApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chocolog.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChocologApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
