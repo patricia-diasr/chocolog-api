fa255b60da984d16d5d3f275f90cbb278856d8b8
Patrícia Dias Rodrigues <patricia.d.rodrigues345@gmail.com>
2025-10-15 16:06:04 -0300

    feat: configurar ambiente de dev local com H2

--- src/main/java/com/chocolog/api/config/SecurityConfig.java
+++ src/main/java/com/chocolog/api/config/SecurityConfig.java
@@ -4,12 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+
 @Configuration
-@EnableWebSecurity 
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
@@ -20,11 +23,17 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(csrf -> csrf.disable()) 
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/**").permitAll() 
-                .anyRequest().authenticated()
-            );
+                .csrf(csrf -> csrf
+                        .ignoringRequestMatchers(toH2Console())
+                )
+                .headers(headers -> headers
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
+                )
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(toH2Console()).permitAll()
+                        .requestMatchers("/**").permitAll()
+                        .anyRequest().authenticated()
+                );
         return http.build();
     }
 }
